a8436ff02d202384e1c77fc136ad341b17dbc290
Ryan Topps Lead <rjtopps@gmail.com>
2018-05-26 14:14:58 -0400

    Add project files.

---
+++ DemoApp/TimerClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace DemoApp
+{
+    public static class TimerClass
+    {
+        public static DispatcherTimer timer = new DispatcherTimer();
+        public static int counter { get; set; }
+    }
+}
